8e9da04972831a9cf6889b9ae115c018a6020f86
ys2816@columbia.edu <yunsun.seu@gmail.com>
2015-02-08 20:16:03 -0500

    update apt

--- routes/apartments/index.js
+++ routes/apartments/index.js
@@ -8,10 +8,24 @@ var Apartments = require('../../models/apartments');
 
 exports.findById = function ( req, res, next ) {
   var id = req.params.id;
-  Apartments.findById(id, function(err, result) {
+  Apartments.findById(id, req.query, function(err, result) {
     if (err) return next(err);
     res.status(200).send(result);
   })
+}
+
+exports.getApartments = function ( req, res, next ) {
+    Apartments.getApartments(req.query, function(err, result) {
+        if (err) return next(err);
+        res.status(200).send(result);
+    })
+}
+
+exports.addApartment = function ( req, res, next ) {
+    Apartments.addApartment(req.body, function(err, result) {
+        if (err) return next(err);
+        res.status(200).send(result);
+    })
 }
 
 exports.deleteById = function ( req, res, next ) {
